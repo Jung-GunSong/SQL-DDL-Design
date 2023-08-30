60dcf4e2904208954d21dc4f53e892396afa03f7
Grace Bethany Hoober <grace.hoober66@gmail.com>
2023-08-29 17:25:27 -0700

    Create tables for planets and moons

--- outerspace.sql
+++ outerspace.sql
@@ -25,3 +25,15 @@ CREATE TABLE Stars (
     name VARCHAR(50) NOT NULL,
     temp_in_K INT NOT NULL
 )
+
+CREATE TABLE Planets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    orbital_period_in_years FLOAT NOT NULL,
+    star_id INT NOT NULL REFERENCES Stars
+)
+
+CREATE TABLE Moons (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+)
